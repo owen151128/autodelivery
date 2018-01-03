ba8c5a1f2fc40925aeaea64437fd01f7bc8639ec
owen151128 <owen151128@gmail.com>
2018-01-03 19:34:13 +0900

    add_rx_selector_util

---
+++ app/src/main/java/com/hpcnt/autodelivery/util/RxSelectorEventUtil.java
@@ -0,0 +1,31 @@
+package com.hpcnt.autodelivery.util;
+
+import io.reactivex.Observable;
+import io.reactivex.subjects.BehaviorSubject;
+
+public class RxSelectorEventUtil {
+    private static RxSelectorEventUtil instance;
+    private BehaviorSubject<Integer> selectorSubject;
+
+    private RxSelectorEventUtil() {
+        selectorSubject = BehaviorSubject.create();
+    }
+
+    public static RxSelectorEventUtil getInstance() {
+        if (instance == null)
+            instance = new RxSelectorEventUtil();
+        return instance;
+    }
+
+    public void sendSelectorEvent(int mode) {
+        selectorSubject.onNext(mode);
+    }
+
+    public void close() {
+        selectorSubject.onComplete();
+    }
+
+    public Observable<Integer> receiveSelectorEvent() {
+        return selectorSubject;
+    }
+}
